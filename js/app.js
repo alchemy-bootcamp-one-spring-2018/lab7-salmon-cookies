fa6a2074485dfda64870c20d045bf006cf3c577b
Mark Myers <markalope@gmail.com>
2018-05-09 14:23:25 -0700

    Psuedo code added to files

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-console.log('app.js is loading')
+console.log('app.js is loading');
+
+// Function to create table header
+
+// Function to create table footer
+
+// Function to total up columns
+
+// Loop to create table rows from object array
+
+// Function calls (to all above) to render complete table
--- js/cookiestand.js
+++ js/cookiestand.js
@@ -1,1 +1,7 @@
-console.log('cookiestand.js is loading')
+console.log('cookiestand.js is loading');
+
+/* 
+object constructor to build array of cookie stand locations
+containing an array of times with sales figures for each time slot
+*/
+
