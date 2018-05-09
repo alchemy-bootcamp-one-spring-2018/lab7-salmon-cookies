d7f4f55e225b7322929dc2d467066137d282dd9e
Rehmsy <sarahrehmer@gmail.com>
2018-05-09 16:33:28 -0700

    trying to get more than one cookie property to show up in the row.

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 class Batch {
 
-    constructor(batch, flavor, ounces, retailPrice) {
+    constructor(batch, flavor, ounces, retailPrice, cookiesSold) {
         this.batch = batch;
         this.flavor = flavor;
         this.ounces = ounces;
         this.retailPrice = retailPrice;
+        this.cookiesSold = cookiesSold;
 
     }
 }
 
-let batch1 = new Batch(1, 'Dijon', '3oz', 6.99);
-let batch2 = new Batch(2, 'Garlic Butter', '3.5oz', 7.99);
-let batch3 = new Batch(3, 'Orange Glazed', '4oz', 5.99);
+let batch1 = new Batch(1, 'Dijon', '3oz', 6.99, 62);
+let batch2 = new Batch(2, 'Garlic Butter', '3.5oz', 7.99, 121);
+let batch3 = new Batch(3, 'Orange Glazed', '4oz', 5.99, 79);
 console.log('batch1', batch1);
 console.log('retailPrice', batch1.retailPrice);
 
@@ -21,4 +22,4 @@ function addRow(cookie) {
     tr.textContent = cookie;
     tBody.appendChild(tr);
 }
-addRow(batch1.batch);
+addRow(batch1.batch + batch1.flavor + batch1.ounces);
