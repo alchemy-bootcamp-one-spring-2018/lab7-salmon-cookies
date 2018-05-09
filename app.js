69e2b033110ae838e3d4c8babf582ee0f504c1d3
rbtprograms <rbt61690@gmail.com>
2018-05-09 16:12:15 -0700

    its going well!

--- app.js
+++ app.js
@@ -2,15 +2,19 @@
 // console.log(firstBatch.sold(2));
 // console.log(firstBatch.profitMargin());
 
-const rowElement = document.getElementById('main-table');
+const rowElement = document.getElementById('table-body');
+
 
 for(var i = 0; i < cookieData.length; i++){
-    // console.log(object[i]);
-    console.log('here is my message');
-    var newTr = cookieData[i].createTd();
-    console.log('here is newTd', newTr);
+
+    var newTr = cookieData[i].createTr();
+
+    newTr.setAttribute("id", "row-" + i)
+
     rowElement.appendChild(newTr);
 }
+
+createTh();
 
 
 //createRow(firstBatch);
--- data.js
+++ data.js
@@ -19,7 +19,11 @@ class Batch {
         return this.price - this.cost;
     }
 
-    createTd(){
+    createTh(){
+        const head = document.createElement('th');
+    }
+
+    createTr(){
         const row = document.createElement('tr');
 
         const firstData = document.createElement('td');
@@ -34,6 +38,18 @@ class Batch {
         thirdData.textContent = this.inventory;
         row.appendChild(thirdData);
 
+        const fourthData = document.createElement('td');
+        fourthData.textContent = this.price;
+        row.appendChild(fourthData);
+
+        const fifthData = document.createElement('td');
+        fifthData.textContent = this.cost;
+        row.appendChild(fifthData);
+
+        const sixthData = document.createElement('td');
+        sixthData.textContent = this.cookieSize;
+        row.appendChild(sixthData);
+
         return row;
     }
 }
@@ -41,5 +57,6 @@ class Batch {
 const cookieData = [
     firstBatch = new Batch('1', 'Pacific', 24, 20.00, 7.50, 'Small'),
     secondBatch = new Batch('2', 'Atlantic', 10, 25.00, 8.50, 'Medium'),
-    thirdBatch = new Batch('3', 'Pacific', 7, 30.00, 9.25, 'Large')
+    thirdBatch = new Batch('3', 'Pacific', 7, 30.00, 9.25, 'Large'),
+    fourthBatch = new Batch('4', 'Pacific', 45, 35.00, 10.00, 'Small')
 ];
