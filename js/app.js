b344f0752fd6b2183d37b7b1074798421e556ac9
Mark Myers <markalope@gmail.com>
2018-05-09 16:52:53 -0700

    got daily total function working and tested

--- js/app.js
+++ js/app.js
@@ -1,10 +1,25 @@
 console.log('app.js is loading');
 
+
+
 // Function to create table header
 
 // Function to create table footer
 
-// Function to total up columns
+// Function to total up columns - sales across all locations
+
+let salesByLocation = 0;
+
+let allLocationsSales = function(){
+    for(i = 0; i < allLocations.length - 1; i++ )
+    salesByLocation += allLocations[i].salesPerHour1000; 
+};
+
+allLocationsSales();
+console.log(allLocations[0].dailySales());
+console.log(allLocations[0].salesPerDay);
+console.log(allLocations[0].salesPerHour1000);
+console.log('all location sales value (10:00) is: ' + salesByLocation);
 
 // Loop to create table rows from object array
 
