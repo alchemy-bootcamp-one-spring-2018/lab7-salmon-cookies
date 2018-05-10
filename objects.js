03429a005b10f61046e403316ebc1648c1b90799
Easton Gorishek <ieaston26@gmail.com>
2018-05-10 12:53:53 -0700

    pushing to GitHub

--- objects.js
+++ objects.js
@@ -13,10 +13,9 @@ class CoffeeDrinks {
 }
 
 //instantiating coffee objects to use later in an array <X>
-
-const coffee = new CoffeeDrinks('drip coffee', 'hot', 'small - medium - large', 2.50, 5);
-const americano = new CoffeeDrinks('americano', 'both', 'small - medium - large', 3.00, 5);
-const latte = new CoffeeDrinks('latte', 'both', 'small - medium - large', 3.50, 10);
+const coffee = new CoffeeDrinks('drip coffee', 'hot', 'small - medium- large', 2.50, 5);
+const americano = new CoffeeDrinks('americano', 'hot/iced', 'small - medium - large', 3.00, 5);
+const latte = new CoffeeDrinks('latte', 'hot/iced', 'small - medium - large', 3.50, 10);
 
 
 // creating an array of coffee objects to call upon in my app.js to include in data table <X>
