ec5344e7481575f5c96aed76f7e103ae5b843e05
Mark Myers <markalope@gmail.com>
2018-05-10 00:44:39 -0700

    that\s all I can do for now. Need work on the loops

--- js/app.js
+++ js/app.js
@@ -33,8 +33,8 @@ td = t.content.querySelectorAll("td");
 
 var createFooterRow = function(){
     td[0].textContent = '';
-    td[1].textContent = 'working';
-    td[2].textContent = 'need calc';
+    td[1].textContent = salesByLocation1000;
+    td[2].textContent = salesByLocation1100;
     td[3].textContent = 'need calc';
     td[4].textContent = 'need calc';
     td[5].textContent = 'need calc';
@@ -50,49 +50,58 @@ var createFooterRow = function(){
 
 };
 
-// Function to total up columns - sales across all locations
-
-let salesByLocation = 0;
-
-let allLocationsSales = function(){
-    for(j = 1; j < allLocations.length; j++)
-        salesByLocation += allLocations[j].salesPerHour1000; 
-};
-
-
-console.log('this is the second item in the hours array - should be 11:00: ' + allLocations[0].location[1]);
-
-console.log('all location sales value (10:00) is: ' + salesByLocation[1]);
-
-// Loop to create table rows from object array
+// Function to create table rows for body
 
 t = document.querySelector('#body-rows');
 td = t.content.querySelectorAll("td");
 
 var createLocationRows = function(){
-    td[0].textContent = allLocations[0].location;
-    td[1].textContent = allLocations[0].salesPerHour1000;
-    td[2].textContent = allLocations[0].salesPerHour1100;
-    td[3].textContent = allLocations[0].salesPerHour1200;
-    td[4].textContent = allLocations[0].salesPerHour1300;
-    td[5].textContent = allLocations[0].salesPerHour1400;
-    td[6].textContent = allLocations[0].salesPerHour1500;
-    td[7].textContent = allLocations[0].salesPerHour1600;
-    td[8].textContent = allLocations[0].salesPerHour1700;
-    td[9].textContent = allLocations[0].salesPerHour1800;
+    for(i = 0; i < allLocations.length; i++) {
+        td[0].textContent = allLocations[i].location;
+        td[1].textContent = allLocations[i].salesPerHour1000;
+        td[2].textContent = allLocations[i].salesPerHour1100;
+        td[3].textContent = allLocations[i].salesPerHour1200;
+        td[4].textContent = allLocations[i].salesPerHour1300;
+        td[5].textContent = allLocations[i].salesPerHour1400;
+        td[6].textContent = allLocations[i].salesPerHour1500;
+        td[7].textContent = allLocations[i].salesPerHour1600;
+        td[8].textContent = allLocations[i].salesPerHour1700;
+        td[9].textContent = allLocations[i].salesPerHour1800;
 
-    // Clone the new row and insert it into the table
-    tb = document.querySelector("tbody");
-    clone3 = document.importNode(t.content, true);
-    tb.appendChild(clone3);
+        // Clone the new row and insert it into the table
+        tb = document.querySelector("tbody");
+        clone = document.importNode(t.content, true);
+        tb.appendChild(clone);
 
+    }
 };
+
+// Function to total up columns - sales across all locations
+
+let salesByLocation1000 = 0;
+let salesByLocation1100 = 0;
+
+let allLocationsSales = function(){
+    for(allSales = 0; allSales < allLocations.length; allSales++)
+        salesByLocation1000 += allLocations[allSales].salesPerHour1000; 
+        
+};
+
+
+
+
+console.log('this is the second item in the hours array - should be 11:00: ' + allLocations[0].location[1]);
+
+console.log('all location sales value (10:00) is: ' + allLocations[1].salesPerHour1000);
 
 // Function calls (to all above) to render complete table
 
-allLocationsSales();
+
 createHeaderRow();
 createLocationRows();
+allLocationsSales();
 createFooterRow();
+
+console.log(salesByLocation1000);
 
 
