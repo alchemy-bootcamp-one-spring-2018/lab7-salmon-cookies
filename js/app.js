1ef57681a4eac80d49f924cfb5a6d18674fd631a
Mark Myers <markalope@gmail.com>
2018-05-09 17:32:22 -0700

    got HTML templates working!!!! and created code to dynamically generate the header

--- js/app.js
+++ js/app.js
@@ -3,6 +3,17 @@ console.log('app.js is loading');
 
 
 // Function to create table header
+console.log('this is the second item in the hours array - should be 11:00: ' + allLocations[0].hours[1]);
+
+var t = document.querySelector('#header-row'),
+  td = t.content.querySelectorAll("td");
+  td[0].textContent = allLocations[0].hours[1];
+  td[1].textContent = allLocations[0].hours[2];
+
+  // Clone the new row and insert it into the table
+  var tb = document.querySelector("thead");
+  var clone = document.importNode(t.content, true);
+  tb.appendChild(clone);
 
 // Function to create table footer
 
@@ -11,14 +22,14 @@ console.log('app.js is loading');
 let salesByLocation = 0;
 
 let allLocationsSales = function(){
-    for(i = 0; i < allLocations.length - 1; i++ )
-    salesByLocation += allLocations[i].salesPerHour1000; 
+    for(i = 0; i < allLocations.length - 1; i++)
+        salesByLocation += allLocations[i].salesPerHour1000; 
 };
 
 allLocationsSales();
-console.log(allLocations[0].dailySales());
-console.log(allLocations[0].salesPerDay);
-console.log(allLocations[0].salesPerHour1000);
+
+console.log('this is the second item in the hours array - should be 11:00: ' + allLocations[0].hours[1]);
+
 console.log('all location sales value (10:00) is: ' + salesByLocation);
 
 // Loop to create table rows from object array
