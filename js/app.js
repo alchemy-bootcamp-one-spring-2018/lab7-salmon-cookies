6f35d909f7868647be08bb657d9264188e3b88f5
carriecarter <carrie@carrie-carter.com>
2018-05-09 18:23:08 -0700

    basic functionality complete

--- js/app.js
+++ js/app.js
@@ -1,2 +1,21 @@
 'use strict';
 
+//instantiate the existing table
+var t = document.querySelector('#batch-row'),
+td = t.content.querySelectorAll('td');
+
+
+for (let i = 0; i < allTheCookies.length; i++) {
+    td[0].textContent = allTheCookies[i].batch;
+    td[1].textContent = allTheCookies[i].salmonType;
+    td[2].textContent = allTheCookies[i].size;
+    td[3].textContent = allTheCookies[i].cost;
+    td[4].textContent = allTheCookies[i].price;   
+}    
+
+    //clone the new row and display it in the table
+    var tb = document.querySelector("tbody");
+    var clone = document.importNode(t.content, true);
+    tb.appendChild(clone);
+    
+
