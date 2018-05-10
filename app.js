a54e60b8fb3821c75c5e10827bce161e7ac894f2
Zachar13y <zachar13y@gmail.com>
2018-05-10 08:45:42 -0700

    Added more insert functions in app.js, working on changing variabl names.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+/* global batchList */
+
 function renderCookies() {
     var parent = document.getElementById('cookie-table');
     for(var i = 0; i < batchList.length; i++) {
@@ -5,16 +7,31 @@ function renderCookies() {
         child.id = 'row-' + i;
         parent.appendChild(child);
 
-        insertCost(i);
+        insertChocolateChip(i);
+        insertPrice(i);
+        insertProfit(i);
     }
 }
 renderCookies();
 
-function insertCost(i) {
+function insertChocolateChip(i) {
     var parent = document.getElementById('row-' + i);
     var child = document.createElement('td');
     parent.appendChild(child);
-    child.textContent = batchList[i].cost;
+    child.textContent = batchList[i].ChocolateChip;
 }
 
 
+function insertPrice(i) {
+    var parent = document.getElementById('row-' + i);
+    var child = document.createElement('td');
+    parent.appendChild(child);
+    child.textContent = batchList[i].price;
+}
+
+function insertProfit(i) {
+    var parent = document.getElementById('row-' + i);
+    var child = document.createElement('td');
+    parent.appendChild(child);
+    child.textContent = batchList[i].profit;
+}
--- cookie.js
+++ cookie.js
@@ -1,9 +1,9 @@
 class Batch{
 
-    constructor(batch, cost, price, profit, size, inventory, glipGlop){
+    constructor(batch, chocolateChip, price, profit, size, inventory, glipGlop){
 
         this.batch = batch;
-        this.cost = cost;
+        this.chocolateChip = chocolateChip;
         this.price = price;
         this.profit = profit;
         this.size = size;
@@ -11,28 +11,30 @@ class Batch{
         this.glipGlop = glipGlop;
         
     }
-
-    sell() {
-        console.log(batch1.inventory);
-        return this.inventory--;
-    }
-
-    profit() {
-        return this.price - this.cost;
-    }
 }
 
 let batch1 = new Batch(1, 200, 4, 30000, 'Big', 10, 'Of Course');
 let batch2 = new Batch(2, 225, 4, 29544, 'Big', 10, 'Of Course');
 
 var batchList = [
+
     batch1,
     batch2
-]
+];
 
 
-
+console.log(batchList);
 console.log('batch1', batch1);
-console.log('batch2', batch2);
 
-console.log('hey');
+
+
+// console.log('batch2', batch2);
+
+// sell() {
+//     console.log(batch1.inventory);
+//     return this.inventory--;
+// }
+
+// profit() {
+//     return this.price - this.chocolateChip;
+// }
