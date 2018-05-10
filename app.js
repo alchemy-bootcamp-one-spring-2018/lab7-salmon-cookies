4222d0007c3dcb671d42a712555f1f787e1ae7eb
Zachar13y <zachar13y@gmail.com>
2018-05-09 19:25:26 -0700

    Rendered content to page using insertCost().

--- app.js
+++ app.js
@@ -1,33 +1,20 @@
-class Batch{
+function renderCookies() {
+    var parent = document.getElementById('cookie-table');
+    for(var i = 0; i < batchList.length; i++) {
+        var child = document.createElement('tr');
+        child.id = 'row-' + i;
+        parent.appendChild(child);
 
-    constructor(batch, cost, price, profit, size, inventory, glipGlop){
-
-        this.batch = batch;
-        this.cost = cost;
-        this.price = price;
-        this.profit = profit;
-        this.size = size;
-        this.inventory = inventory;
-        this.glipGlop = glipGlop;
-        
-    }
-
-    sell() {
-        console.log(batch1.inventory);
-        return this.inventory--;
-    }
-
-    profit() {
-        return this.price - this.cost;
+        insertCost(i);
     }
 }
+renderCookies();
 
-let batch1 = new Batch(1, 200, 4, 30000, 'Big', 10, 'Of Course');
-let batch2 = new Batch(2, 225, 4, 29544, 'Big', 10, 'Of Course');
+function insertCost(i) {
+    var parent = document.getElementById('row-' + i);
+    var child = document.createElement('td');
+    parent.appendChild(child);
+    child.textContent = batchList[i].cost;
+}
 
 
-
-console.log('batch1', batch1);
-console.log('batch2', batch2);
-
-console.log('hey');
---
+++ cookie.js
@@ -0,0 +1,38 @@
+class Batch{
+
+    constructor(batch, cost, price, profit, size, inventory, glipGlop){
+
+        this.batch = batch;
+        this.cost = cost;
+        this.price = price;
+        this.profit = profit;
+        this.size = size;
+        this.inventory = inventory;
+        this.glipGlop = glipGlop;
+        
+    }
+
+    sell() {
+        console.log(batch1.inventory);
+        return this.inventory--;
+    }
+
+    profit() {
+        return this.price - this.cost;
+    }
+}
+
+let batch1 = new Batch(1, 200, 4, 30000, 'Big', 10, 'Of Course');
+let batch2 = new Batch(2, 225, 4, 29544, 'Big', 10, 'Of Course');
+
+var batchList = [
+    batch1,
+    batch2
+]
+
+
+
+console.log('batch1', batch1);
+console.log('batch2', batch2);
+
+console.log('hey');
