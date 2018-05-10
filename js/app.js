5ea9240a3f2bb75276f49b438f31e5d100ee3155
Ryan Khatib <ryan.khatib@gmail.com>
2018-05-09 17:47:13 -0700

    added batch array

--- js/app.js
+++ js/app.js
@@ -1,42 +1,54 @@
 'use strict';
-/* exported showData, tableArray*/
+/* exported Batch, batch1, batch2, batch3, batch4, batch5, tableArray, createTableData, createRows*/
 
-function createRows(){
-    
-    properties = [
-        this.batch,
-        this.salmonType,
-        this.size,
-        this.inventory,
-        this.cost,
-        this.batchPrice,
-        this.unitPrice,
-    ]
-
-    var genRow = document.getElementById('table-data');
-    var tr = document.createElement('tr');
-
-    for(let i = 0; i < .length; i++) {
-        var td = tableArray[i].createElement('td');
-
-        genRow.appendChild(tr);
-        tr.appendChild(td);
-
+class Batch {
+    constructor(batch, salmonType, size, inventory, cost, batchPrice, unitPrice) {
+        this.batch = batch,
+        this.salmonType = salmonType,
+        this.size = size,
+        this.inventory = inventory,
+        this.cost = cost,
+        this.batchPrice = batchPrice,
+        this.unitPrice = unitPrice;
     }
+
+    sell() {
+        return this.inventory--;
+    }
+
+    bake(count) {
+        return this.inventory + count;
+    }
+
+    profit() {
+        return this.price - this.cost;
+    }
+
 }
 
 
+let batch1 = new Batch (1, 'Pacific', 'M', 8, 10, 20, 2.5);
+console.log ('batch1', batch1);
 
-//     function fruitShow(){
-//         var fruit = document.getElementById('fruit-list');
-//         console.log(fruit);
-//         for(var i = 0; i < fruitStand.length; i++) {
-//             var createListEle = fruitStand[i].createLi();
-//             fruit.appendChild(createListEle);
-//             var genImg = fruitStand[i].showImage();
-//             fruit.appendChild(genImg);
-// }
+let batch2 = new Batch (2, 'Atlantic', 'S', 12, 10, 20, 2.0);
+console.log ('batch2', batch2);
 
+let batch3 = new Batch (3, 'Pacific', 'M', 8, 10, 20, 2.5);
+console.log ('batch3', batch3);
 
+let batch4 = new Batch (4, 'Pacific', 'S', 12, 10, 20, 2.0);
+console.log ('batch4', batch4);
 
-createRows();
+const batchArray = [batch1, batch2, batch3, batch4];
+console.log(batchArray, 'batchArray');
+
+function createRows() {
+
+    var getHead = document.querySelector('#cookie-table');
+    var tHead = document.createElement('thead');
+    console.log('this works');
+    for(let i = 0; i < 6; i++) {
+        getHead.appendChild(tHead);
+    }
+}
+
