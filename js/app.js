679628b951558e07fb971077c3174484a4185b40
Kevin Grayson <k.grayson.ms@gmail.com>
2018-05-09 21:33:53 -0700

    createHeader, getTitles, createBody, createFooter all working

--- js/app.js
+++ js/app.js
@@ -7,12 +7,27 @@ function createHeader() {
     var thead = document.createElement('thead');
     table.appendChild(thead);
     //document.getElementsByTagName('thead')[0].textContent = 'This is some text';
+}
 
-    for(var i = 0; i < arrHeaders.length; i++) {
-    
-    }
+function getTitles() {
+        var thead = document.getElementsByTagName('thead')[0];
 
-    //var tHeadInput = document.getElementsByTagName('thead');
+        var row = thead.insertRow(0);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3);
+        var cell5 = row.insertCell(4);
+        var cell6 = row.insertCell(5);
+
+        cell1.textContent = arrHeaders[0];
+        cell2.textContent = arrHeaders[1];
+        cell3.textContent = arrHeaders[2];
+        cell4.textContent = arrHeaders[3];
+        cell5.textContent = arrHeaders[4];
+        cell6.textContent = arrHeaders[5];
+
+        return thead;
 }
 
 function createBody() {
@@ -40,5 +55,6 @@ function createFooter() {
 }
 
 createHeader();
+getTitles();
 createBody();
 createFooter();
--- js/stores.js
+++ js/stores.js
@@ -1,5 +1,5 @@
 'use strict';
-/* globals Store arrHeaders */
+/* globals Store */
 /* exported sell, profit, render, getTitles */
 
 class Store {
@@ -25,42 +25,21 @@ class Store {
         var tbody = document.getElementsByTagName('tbody')[0];
 
         var row = tbody.insertRow(0);
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        var cell3 = row.insertCell(2);
-        var cell4 = row.insertCell(3);
-        var cell5 = row.insertCell(4);
-        var cell6 = row.insertCell(5);
+        var cell0 = row.insertCell(0);
+        var cell1 = row.insertCell(1);
+        var cell2 = row.insertCell(2);
+        var cell3 = row.insertCell(3);
+        var cell4 = row.insertCell(4);
+        var cell5 = row.insertCell(5);
 
-        cell1.textContent = this.location;
-        cell2.textContent = this.salmonType;
-        cell3.textContent = this.size;
-        cell4.textContent = this.cost;
-        cell5.textContent = this.price;
-        cell6.textContent = this.inventory;
+        cell0.textContent = this.location;
+        cell1.textContent = this.salmonType;
+        cell2.textContent = this.size;
+        cell3.textContent = this.cost;
+        cell4.textContent = this.price;
+        cell5.textContent = this.inventory;
 
         return tbody;
-    }
-
-    getTitles() {
-        var thead = document.getElementsByTagName('thead')[0];
-
-        var row = thead.insertRow(0);
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        var cell3 = row.insertCell(2);
-        var cell4 = row.insertCell(3);
-        var cell5 = row.insertCell(4);
-        var cell6 = row.insertCell(5);
-
-        cell1.textContent = this.location;
-        cell2.textContent = this.salmonType;
-        cell3.textContent = this.size;
-        cell4.textContent = this.cost;
-        cell5.textContent = this.price;
-        cell6.textContent = this.inventory;
-
-        return thead;
     }
 }
 
